ff73dcbd56d7c3bd2b90b0934ff7ca99aee6f36c
UT0903 <b07902048@ntu.edu.tw>
2022-03-04 19:16:03 +0800

    new megaopus

--- draw_plt.py
+++ draw_plt.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
-
-data_num = [0.1, 0.5, 0.7,  1, 5, 10, 20, 50, 70, 100, 150]
+import numpy as np
+data_num = np.array([0.1, 0.5, 0.7,  1, 5, 10, 20, 50, 70, 100, 150]) * 880788 / 1e6
+print(data_num)
 spend_time = [14, 42, 65, 8, 26, 48, 55, 119, 228, 415, 715]
 plt.plot(data_num, spend_time, marker='o')
-plt.xlabel('data size (times, base size=880788, eg: 100*880788=88078800)')
+plt.xlabel('data size (1e6)')
 plt.ylabel('elapsed time (s)')
 plt.savefig('performance.png')
 plt.show()
